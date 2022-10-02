44243ae76aad8011c9a9def9d2669e416e8124a8
The_Legend_of_xD (kibo64) <61091898+TheLegendofxD@users.noreply.github.com>
2022-10-02 05:40:24 +0000

    Added a working module system

--- js/app.js
+++ js/app.js
@@ -18,4 +18,93 @@ function play_beep(direction = 'both') {
     beepAudio.play();
 }
 
-/* Creating Page */
+/* ----- Creating Page ----- */
+var modules = [
+    document.getElementById('template_stereo_test'),
+    document.getElementById('template_bass_test')
+];
+
+function get_module_order() {
+    var _module_order = [0,1];
+
+
+    /* Checking If Module Order is Valid */
+    if (localStorage.getItem('module_order') != null) {
+        let temp = localStorage.getItem('module_order').split(',');
+        let success = true;
+
+        
+        for (var i = 0; i < temp.length; i++) {
+            if (!(temp[i] > -1 && temp[i] < _module_order.length)) {
+                success = false;
+                break;
+            }
+        }
+
+        if (success) { _module_order = temp; };
+    }
+
+    return _module_order;
+}
+
+function set_module_order(_module_order) {
+    var mo_string = '';
+    for (var i = 0; i < _module_order.length; i++) {
+        mo_string += _module_order[i].toString();
+        if (i != _module_order.length - 1) {
+            mo_string += ',';
+        }
+    }
+    localStorage.setItem('module_order', mo_string);
+}
+
+function render_modules() {
+    var module_order = get_module_order();
+    const insert_pos = document.getElementById('insert');
+
+    insert_pos.innerHTML = '';
+
+    /* Appending modules */
+    for (var i = 0; i < module_order.length; i++) {
+        var mdl = insert_pos.appendChild(modules[module_order[i]].content.cloneNode(true));
+
+        var mdls = insert_pos.querySelectorAll('.content');
+        var mdl = mdls[mdls.length - 1];
+
+        console.log(mdl, mdls);
+
+        mdl.innerHTML = mdl.innerHTML.replace(/#i0/gi, i);
+        mdl.innerHTML = mdl.innerHTML.replace(/#i1/gi, module_order[i]);
+    }
+
+    feather.replace();
+}
+
+function move_module(display_id, type_id, action) {
+    var module_order = get_module_order();
+
+    if (action == 'up' && display_id > 0) {
+        let temp = module_order[display_id - 1];
+        module_order[display_id - 1] = type_id;
+        module_order[display_id] = temp;
+    } else if (action == 'down' && display_id < module_order.length) {
+        let temp = module_order[display_id + 1];
+        module_order[display_id + 1] = type_id;
+        module_order[display_id] = temp;
+    } else if (action == 'away') {
+        let temp = [];
+        for (var i = 0; i < module_order.length; i++) {
+            if (i == display_id) {
+                continue;
+            }
+            temp.push(module_order[i]);
+        }
+        module_order = temp;
+    }
+
+    set_module_order(module_order);
+    /*render_modules(); // This causes the bug, that the old link in the context menu still remains */
+    window.location.reload()
+}
+
+render_modules();
