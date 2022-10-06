b7c176a0fe4414e4af7697489c0039de21b568f4
The_Legend_of_xD (kibo64) <61091898+TheLegendofxD@users.noreply.github.com>
2022-10-06 18:35:00 +0000

    increased version number to 1.4

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const ls_prefix = 'f384b3_';
-const version = [13, '1.3-release', '05/10/22'];
+const version = [14, '1.4-release', '06/10/22'];
 
 const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
 const audioPan = audioCtx.createStereoPanner();
